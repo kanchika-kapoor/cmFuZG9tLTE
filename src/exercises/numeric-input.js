0d121654e35d1a521da808d928a67126583dda2d
kanchika-kapoor <kanchika.ai@gmail.com>
2024-01-13 20:23:32 +0530

    added numeric input component

--- src/exercises/numeric-input.js
+++ src/exercises/numeric-input.js
@@ -21,8 +21,52 @@
 const NumericInput = {
   init: () => {
     document.querySelectorAll('.c-numeric-input').forEach(elem => {
-      console.log('TODO: Please see the above requirement for numeric input');
+      // console.log('TODO: Please see the above requirement for numeric input');
+      elem.addEventListener('blur', NumericInput.onBlur)
+      elem.addEventListener('focus', NumericInput.resetInput)
     });
+  },
+  validateInput: (e)=>{
+    // regex check for negative, decimal numbers
+    const isValidNumber = /^-?\d*\.?\d*$/.test(e.value);
+    return isValidNumber;
+  },
+  resetInput: (e)=>{
+    let elem = e.target
+    elem.className = 'c-numeric-input'
+
+    if(elem.nextSibling.className=='c-numeric-input__error-msg'){
+      elem.nextSibling.remove()
+    }
+  },
+  onBlur: (e) => {
+    let elem = e.target;
+    const isValid = NumericInput.validateInput(elem);
+    NumericInput.resetInput(e);
+
+    if(elem.value && isValid){
+      // format value for leading zeros and decimal
+      elem.value = elem.value.replace(/^0+/, '');
+
+      if (elem.value.charAt(0) === '.') {
+        elem.value = '0' + elem.value;
+      }
+      elem.classList.add('c-numeric-input--valid');
+
+    }else if(elem.value && !isValid){
+      let msgContainer = document.createElement('span');
+      msgContainer.classList.add('c-numeric-input__error-msg');
+      msgContainer.innerHTML = 'invalid input';
+
+      //add classname and append error message span
+      elem.classList.add('c-numeric-input--error');
+
+
+      if(elem.nextSibling.className!='c-numeric-input__error-msg'){
+        elem.after(msgContainer)
+      }
+
+    }
   }
 };
 document.addEventListener('DOMContentLoaded', NumericInput.init);
